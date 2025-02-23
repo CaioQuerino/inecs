d8dae13979da86034af131b83c900548bef51f20
CaioQuerino <caioquerino04@gmail.com>
2025-02-23 15:15:11 -0300

    23-02-2025 : SEO

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { FooterContainer } from '../styles.js'; // Importando estilos
+import { FooterContainer, NavLink } from '../styles.js'; // Importando estilos
+import { Link } from 'react-router-dom';
 
 export function Footer() {
   return (
     <FooterContainer>
       <p>&copy; 2025 INECS - Instituto de Desenvolvimento Social. Todos os direitos reservados.</p>
-      <p>Contato: contato@inecs.org.br | (21) 98191-0324 | Av. Beira Mar, 262, Grupo 203, </p> 
+      <p>Contato: contato@inecs.org.br | <NavLink><Link target='_blank' to='https://wa.me/55981910324'>(21) 98191-0324</Link></NavLink> | Av. Beira Mar, 262, Grupo 203, </p> 
     </FooterContainer>
   );
 }
